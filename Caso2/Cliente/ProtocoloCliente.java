6adf558acc60e4cf5f7695d6fa5190a283fd1d7a
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2025-04-13 15:31:06 -0500

    ProtocoloClientes

--- Caso2/Cliente/ProtocoloCliente.java
+++ Caso2/Cliente/ProtocoloCliente.java
@@ -1,5 +1,33 @@
 package Caso2.Cliente;
 
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+
 public class ProtocoloCliente {
-    
+    public static void procesar (BufferedReader stdIn, BufferedReader lector, PrintWriter escritor) throws Exception {
+        String fromServer;
+        String fromUser;
+
+        boolean ejecutar = true;
+        //se ejecuta el protocolo de comunicación
+        while (ejecutar) {
+            //enviar al servidor
+            System.out.print("Mensaje para enviar: ");
+            fromUser = stdIn.readLine(); 
+
+            if(fromUser != null){ 
+                System.out.println("Cliente: " + fromUser); 
+                if(fromUser.equalsIgnoreCase("OK")){ 
+                    System.out.println("Cliente: Fin de la comunicación"); 
+                    ejecutar = false; 
+                } else {
+                    escritor.println(fromUser); //enviar al servidor
+                }
+            }
+            if((fromServer = lector.readLine()) != null){ 
+                System.out.println("Servidor: " + fromServer); 
+            }
+        }
+
+    }
 }
